13fce8b2bd63b62a595e597e155c6001cd7438c8
telanxu <telanx@gmail.com>
2024-04-07 07:15:05 +0800

    Update custom-script-2.sh

--- custom-script-2.sh
+++ custom-script-2.sh
@@ -18,7 +18,7 @@
 #git apply example.patch
 
 # Modify WiFi ON
-sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Modify default SSID
 # sed -i 's/ssid=OpenWrt/ssid=AirPort/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -27,7 +27,7 @@ sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac802
 sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
 
 # Change Host Name (Pure Numbers or Chinese)
-sed -i 's/OpenWrt/router/g' package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/router/g' package/base-files/files/bin/config_generate
 
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-argon）
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
